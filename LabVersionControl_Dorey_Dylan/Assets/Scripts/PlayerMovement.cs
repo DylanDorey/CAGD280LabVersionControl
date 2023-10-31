f5b1fd735e735808f06759c803f6739b82dec792
DylanDorey <129922313+DylanDorey@users.noreply.github.com>
2023-10-31 11:38:58 -0700

    Updated scene and player controller

--- LabVersionControl_Dorey_Dylan/Assets/Scripts/PlayerMovement.cs
+++ LabVersionControl_Dorey_Dylan/Assets/Scripts/PlayerMovement.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Vector3 moveDirection;
+
+    private int speed = 10;
+
+    public void Jump()
+    {
+        //player can jump
+    }
+
+    public void Movement()
+    {
+        //moves the player
+    }
+
     //Player movement is done
 }
