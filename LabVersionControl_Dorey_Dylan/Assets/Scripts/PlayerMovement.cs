2258c061622bc906723c754a99ce74461aa70c16
DylanDorey <129922313+DylanDorey@users.noreply.github.com>
2023-10-31 11:36:15 -0700

    updated player movement

--- LabVersionControl_Dorey_Dylan/Assets/Scripts/PlayerMovement.cs
+++ LabVersionControl_Dorey_Dylan/Assets/Scripts/PlayerMovement.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    
+    private void Move()
+    {
+        //moves the player
+    }
+
     //Player movement is done
 }
